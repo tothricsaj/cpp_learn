7cb3f8a44799e13dcddd734ec2d67d600092bb29
tothricsaj <tothricsaj@gmail.com>
2026-06-25 12:06:57 +0200

    feat: added name

--- examples/object_lifecycle/TimerClass.cpp
+++ examples/object_lifecycle/TimerClass.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
+#include <cstring>
 #include <sys/time.h>
 #include <unistd.h>
+#include <utility>
 
 class TimerClass {
   public:
@@ -9,42 +11,72 @@ class TimerClass {
       if (gettimeofday(&now, nullptr) == 0) {
         timestamp = now.tv_sec;
       }
+
+      name = new char[3];
+      name[0] = 0;
     }
 
-    TimerClass(const TimerClass& t_other) :
-      timestamp { t_other.timestamp }
-    {}
+    // TimerClass(const TimerClass& t_other) :
+    //   timestamp { t_other.timestamp }
+    // {}
 
-    TimerClass operator=(const TimerClass& t_other) {
-      if (&t_other == this) return *this;
+    // TimerClass& operator=(const TimerClass& t_other) {
+    //   if (&t_other == this) return *this;
 
-      timestamp = t_other.timestamp;
+    //   timestamp = t_other.timestamp;
       
-      return *this;
-    }
+    //   return *this;
+    // }
+
+    // TimerClass(TimerClass&& t_other) noexcept :
+    //   timestamp { t_other.timestamp }
+    // {
+    //   t_other.timestamp = 0;
+    // }
+
+    // TimerClass& operator=(TimerClass&& t_other) {
+    //   timestamp = t_other.timestamp;
+    //   t_other.timestamp = 0;
+
+    //   return *this;
+    // }
 
     ~TimerClass() {
       timeval now;
       if (gettimeofday(&now, nullptr) == 0) {
-        printf("Lifetime of the timer(in seconds) >> %ld\n", now.tv_sec - timestamp);
-      }     
+        printf("Lifetime of %s timer(in seconds) >> %ld\n", name, now.tv_sec - timestamp);
+      }
+
+      delete[] name;
     }
 
     void print() {
       printf("timestamp >> %ld\n", timestamp);
     }
 
+    bool add_name(const char* x) {
+      std::strncpy(name, x, 3);
+
+      return true;
+    }
+
   private:
     time_t timestamp;
+    char* name;
+    size_t name_size;
 };
 
 int main() {
   TimerClass t_1{};
-  TimerClass t_2{ t_1 };
+  TimerClass t_2{};
 
-  usleep(5000000);
+  usleep(1000000);
 
   TimerClass t_3{};
+
+  t_1.add_name("t_1");
+  t_2.add_name("t_2");
+  t_3.add_name("t_3");
 
   printf("t_1 >> ");
   t_1.print();
@@ -53,8 +85,8 @@ int main() {
   printf("t_3 >> ");
   t_3.print();
 
-  t_3 = t_2;
+  // t_3 = std::move(t_2);
 
-  printf("t_3 after copy assignment >> ");
-  t_3.print();
+  // printf("t_3 after move assignment >> ");
+  // t_3.print();
 }
