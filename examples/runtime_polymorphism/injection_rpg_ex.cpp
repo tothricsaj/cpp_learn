d986b6d9c0d40e09bb4e0d23d511a9ea2b2511c2
tothricsaj <tothricsaj@gmail.com>
2026-06-30 09:06:48 +0200

    feat: added injection example with rpg style

---
+++ examples/runtime_polymorphism/injection_rpg_ex.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+
+class Attack {
+  public:
+    const virtual char* make_attack() const =0;
+
+    virtual ~Attack() = default; 
+};
+
+class ArrowAttack : public Attack {
+  public:
+    const char* make_attack() const override {
+      return "shoot the arrow -->";
+    }
+};
+
+class SwordAttack : public Attack {
+  public:
+    const char* make_attack() const override {
+      return "swing the sword";
+    }
+};
+
+class Hero {
+  public:
+    Hero(Attack& attack_handler) : attack_handler{ attack_handler } {};
+
+    void attack() {
+      printf("Hero attacks and %s\n", attack_handler.make_attack());
+    }
+
+  private:
+    Attack& attack_handler;
+};
+
+int main() {
+  ArrowAttack arrow_attack;
+  SwordAttack sword_attack;
+
+  Hero arrow_hero{ arrow_attack };
+  Hero sword_hero{ sword_attack };
+
+  arrow_hero.attack();
+  sword_hero.attack();
+}
