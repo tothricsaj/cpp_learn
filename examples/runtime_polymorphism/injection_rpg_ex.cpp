2380ed7eb3199f95f857bee7f57ba01e36f92d52
tothricsaj <tothricsaj@gmail.com>
2026-06-30 09:10:51 +0200

    refact: injection via poiters:

--- examples/runtime_polymorphism/injection_rpg_ex.cpp
+++ examples/runtime_polymorphism/injection_rpg_ex.cpp
@@ -23,23 +23,26 @@ class SwordAttack : public Attack {
 
 class Hero {
   public:
-    Hero(Attack& attack_handler) : attack_handler{ attack_handler } {};
+    Hero(Attack* attack_handler) : attack_handler{ attack_handler } {};
 
     void attack() {
-      printf("Hero attacks and %s\n", attack_handler.make_attack());
+      printf("Hero attacks and %s\n", attack_handler->make_attack());
     }
 
   private:
-    Attack& attack_handler;
+    Attack* attack_handler;
 };
 
 int main() {
-  ArrowAttack arrow_attack;
-  SwordAttack sword_attack;
+  ArrowAttack* arrow_attack{ new ArrowAttack };
+  SwordAttack* sword_attack{ new SwordAttack };
 
   Hero arrow_hero{ arrow_attack };
   Hero sword_hero{ sword_attack };
 
   arrow_hero.attack();
   sword_hero.attack();
+
+  delete arrow_attack;
+  delete sword_attack;
 }
